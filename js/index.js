d76c48bf761fa3f8d71c208d56fd27a6df0b36cd
GEAN-SS <gerardo.sarmiento150900@gmail.com>
2021-01-03 22:05:45 -0600

    seccion 15 JAVASCRIPT (primer programa)

--- js/index.js
+++ js/index.js
@@ -1,1 +1,4 @@
-log.("hola mundo")
+//seccion 15 JAVASCRIPT (primer programa)
+console.log('Hola Mundo')
+
+// la direccion de la carpeta es C:\Users\Gerardo Sarmiento\github\CursoDeJsHtmlCssNodeJsUdemy\js>
