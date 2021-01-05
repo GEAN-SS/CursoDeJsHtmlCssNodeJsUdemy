2714a83e2a058b2322150c56ea14000516d4e43c
GEAN-SS <gerardo.sarmiento150900@gmail.com>
2021-01-04 19:20:03 -0600

    seccion 15 JAVASCRIPT (tipos de datos/ primera variable / re-asignado valor a una variable)

--- js/index.js
+++ js/index.js
@@ -1,4 +1,24 @@
-//seccion 15 JAVASCRIPT (primer programa)
-console.log('Hola Mundo')
-
+//seccion 15 JAVASCRIPT (tipos de datos/ primera variable / re-asignado valor a una variable)
 // la direccion de la carpeta es C:\Users\Gerardo Sarmiento\github\CursoDeJsHtmlCssNodeJsUdemy\js>
+console.log('Hola Mundo')
+//tipos de datos
+//String: cadena de caracteres. 'a' 'hola' 'hola mundo 34' "a" "hola" "hola mundo 34"
+//Boolean: solo da true o false.
+//Null: es nulo, la variable esta vacia.
+//Number: cualquier numero en JAVASCRIPT 1, 23, 234.
+//Undefined: una variable que no ha sido definida.
+//Object; un objeto.
+
+//definicion de variables
+//var: es algo que se define al inicio de las definiciones de avriables, pero no es aconsejable usarla ya que no es tan util como en otros lengiajes, hay que evitar de usarla lo mas posible
+//var miPrimeraVariable= 'lala'
+
+//let : es una variables que vamos a poder definir y usarla mas adelante sin saber que valor contien esta
+let miPrimeraVariable= 'Mi Primera Variable!'
+console.log(miPrimeraVariable);
+
+//cambiamo el valor de la variable
+miPrimeraVariable= 'Esto ha cambiado'
+console.log(miPrimeraVariable);
+
+//const
