d6117bf27583a1e8c021034da54dc5e7eceecb8f
GEAN-SS <gerardo.sarmiento150900@gmail.com>
2021-01-11 23:20:46 -0600

    seccion 19 y DOM (preparando nuestra aplicacion)

--- intro-DOM/main.js
+++ intro-DOM/main.js
@@ -1,5 +1,4 @@
-// seccion 19 y DOM (agregando elementos html a otro elemento html )
+// seccion 19 y DOM (preparando nuestra aplicacion)
 window.onload= () => {
-  const parrafo=document.getElementById('text')
-  parrafo.innerHTML = '<li>elemento 1</li><li>elemento 2</li>'
-}       //cambiamos el innerText por innerHTML
+
+}
